e049d72cfc582b84612981c4a479c9d758f41282
Bypass <gramuser332@gmail.com>
2025-03-26 02:25:04 -0300

    Adicionando módulos do projeto

---
+++ modules/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Pacote de módulos para o WiFi Scanner e Brute Force Tool.
+Contém os módulos para escaneamento de rede e ataques de brute force.
+"""
+
+__version__ = '1.0.0'
